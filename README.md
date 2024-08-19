14050620467255a1870c05f18a923415cb4cc91c
CJavat <cdpm98@hotmail.com>
2024-08-18 19:04:38 -0600

    FIRST COMMIT

---
+++ README.md
@@ -0,0 +1,9 @@
+# Backend en Neste
+
+1. Ejecutar
+
+```
+docker compose up -d
+```
+
+2. Copiar el `.env.template` y renombrarlo a `.env`
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,105 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { LoginResponse } from './interfaces/login-response.interface';
+import { RegisterUserDto } from './dto/register-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      // 1. Encriptar la contraseña
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+
+      // 2. Guardar el usuario en la base de datos
+      await newUser.save();
+
+      // 3. Guardar el JWT
+
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
+    } catch (error) {
+      if (error.code === 11000)
+        throw new BadRequestException(`${createUserDto.email} already exists`);
+
+      throw new InternalServerErrorException('Something terrible happened');
+    }
+  }
+
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email: email });
+    if (!user) throw new UnauthorizedException('Invalid Email ');
+    if (!bcryptjs.compareSync(password, user.password))
+      throw new UnauthorizedException('Invalid Password');
+
+    const { password: _, ...rest } = user.toJSON();
+
+    return {
+      user: rest,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
+  async register(registerUserDto: RegisterUserDto): Promise<LoginResponse> {
+    const user = await this.create(registerUserDto);
+
+    return {
+      user: user,
+      token: this.getJwtToken({ id: user._id }),
+    };
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+
+  async findUserById(id: string) {
+    const user = await this.userModel.findById(id);
+    const { password: _, ...rest } = user.toJSON();
+
+    return rest;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+}
